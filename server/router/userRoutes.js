ab4aeba86f0958e597ccaf41e2156dad0fd921cf
Arne-dub <debarrun@gmail.com>
2023-07-22 02:23:11 +0530

    bla

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -4,6 +4,6 @@ const loginControl = require('../controller/loginController')
 
 router.route('/signup').post(loginControl.signup)
 router.route('/login').post(loginControl.login)
-router.route('/forgetPass').post(loginControl.verify,loginControl.forgotPassword)
+// router.route('/forgetPass').post(loginControl.verify,loginControl.forgotPassword)
 
 module.exports = router
