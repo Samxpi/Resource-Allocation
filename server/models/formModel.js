238ad53cec32f644e14fec4b0fa8ddfab0f97e89
Arne-dub <debarrun@gmail.com>
2023-07-21 22:49:39 +0530

    formId added

--- server/models/formModel.js
+++ server/models/formModel.js
@@ -1,51 +1,67 @@
-const mongoose = require("mongoose") ;
-const dater =() =>{
-    let d = Date();
-    let a = d.toString();
-    return a;
-}
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const dater = () => {
+  let d = Date();
+  let a = d.toString();
+  return a;
+};
 
 const formSchema = new mongoose.Schema({
-    resourceName:{
-        type:String,
-        required:true
+  formID: {
+    type: String,
+    required: true,
+    unique: true,
+    default: () => uuidv4(),
+  },
+  resourceName: {
+    type: String,
+    required: true,
+  },
+  eventName: {
+    type: String,
+    required: true,
+  },
+  eventDetails: {
+    type: String,
+    required: true,
+  },
+  approvedTime: {
+    type: String,
+    default: dater,
+    select: false,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (num) {
+        // Convert the number to a string and check its length
+        const phoneNumberString = num.toString();
+        return phoneNumberString.length >= 10;
+      },
+      message: "Phone number must be at least 10 digits long",
     },
-    eventName:{
-        type: String,
-        required: true,
-    },
-    eventDetails:{
-        type: String,
-        required: true,
-    },
-    approvedTime:{
-        type: String,
-        default: dater,
-        select: false,
-    },
-    phoneNumber: {
-        type: Number,
-        required: true,
-    },
-    startDate:{
-        type: Date,
-        required: true,
-    },
-    endDate:{
-        type: Date,
-        required: true,
-    },
-    Technician: {
-        type: Boolean,
-    },
-    Cleaning: {
-        type: Boolean,
-    },
-    Sound: {
-        type: Boolean,
-    }
-  });
-  
-  const form = mongoose.model("forms", formSchema);
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  Technician: {
+    type: Boolean,
+  },
+  Cleaning: {
+    type: Boolean,
+  },
+  Sound: {
+    type: Boolean,
+  },
+});
 
-  module.exports = form;
+const form = mongoose.model("forms", formSchema);
+
+module.exports = form;
