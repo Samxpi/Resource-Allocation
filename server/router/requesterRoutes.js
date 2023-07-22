0a6274f567eae2d491bffe1290c2062f9d8fc684
Arne-dub <debarrun@gmail.com>
2023-07-22 12:11:18 +0530

    status not working

--- server/router/requesterRoutes.js
+++ server/router/requesterRoutes.js
@@ -7,5 +7,7 @@ const formControl = require('../controller/formController')
 router.route('/home').post(formControl.forms)
 router.route('/home/:id').delete(formControl.deleteForm)
 router.route('/approver').post(formControl.sortForm)
+router.route('/status/:id').patch(formControl.changeFormStatus) // not working need to be tested
+
 
 module.exports = router
