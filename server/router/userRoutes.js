fd8032e1ef7da60091a2eb6cec47bc45e9877ca5
Arne-dub <debarrun@gmail.com>
2023-07-22 11:45:26 +0530

    forget and reset route added

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -4,6 +4,7 @@ const loginControl = require('../controller/loginController')
 
 router.route('/signup').post(loginControl.signup)
 router.route('/login').post(loginControl.login)
-// router.route('/forgetPass').post(loginControl.verify,loginControl.forgotPassword)
+router.route('/forget-Password').post(loginControl.verify,loginControl.forgotPassword)
+router.route('/reset-Password').post(loginControl.verify,loginControl.resetPassword)
 
 module.exports = router
